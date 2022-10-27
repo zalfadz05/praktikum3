522ecb0600fe2da57dc981bc7ff9989ec936f904
zalfadz05 <115516617+zalfadz05@users.noreply.github.com>
2022-10-27 13:11:36 +0700

    Add files via upload

---
+++ Belah ketupat.py
@@ -0,0 +1,49 @@
+string = ""
+print("Nama : Zalfa Dewi Zahrani")
+print("Nim : 312210320")
+print("Kelas : TI.22.A3")
+x = 5
+bar = x
+# Looping Baris
+while bar >= 0:
+	# Looping Kolom Spasi Kosong
+	kol = bar
+	while kol > 0:
+		string = string + "   "
+		kol = kol - 1
+	# Looping Kolom Bintang Sisi Kiri
+	kiri = 1
+	while kiri < (x - (bar-1)):
+		string = string + " * "
+		kiri = kiri + 1
+	# Looping Kolom Bintang Sisi Kanan
+	kanan = 1
+	while kanan < kiri -1:
+		string = string + " * "
+		kanan = kanan + 1
+
+	string = string + "\n\n"
+	bar = bar - 1
+
+bar = 1
+# Looping Baris
+while bar <= x:
+	kol = bar+1
+	# Looping Kolom Spasi Kosong
+	while kol > 1:
+		string = string + "   "
+		kol = kol - 1
+	# Looping Kolom Bintang Sisi Kiri
+	kiri = 0
+	while kiri < (x - bar):
+		string = string + " * "
+		kiri = kiri + 1
+	# Looping Kolom Bintang Sisi Kanan
+	kanan = kiri
+	while kanan > 1:
+		string = string + " * "
+		kanan = kanan - 1
+
+	string = string + "\n\n"
+	bar = bar + 1
+print (string)
